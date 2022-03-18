c0489cfb125534a4a3cefa01fa3e3bfb303a1b87
Axel KLEIN <axelklein2@gmail.com>
2022-03-18 12:12:09 -0400

    Tests+bot mouvement 3d #6

--- Assets/Scenes/Scripts/MoveToGoalAgent.cs
+++ Assets/Scenes/Scripts/MoveToGoalAgent.cs
@@ -29,7 +29,7 @@ public class MoveToGoalAgent : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
         float moveSpeed = 4f;
-        transform.localPosition += new Vector3(moveX, 0, moveZ)*Time.deltaTime*moveSpeed;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
     public override void Heuristic(in ActionBuffers actionsOut)
     {
